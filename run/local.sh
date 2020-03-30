3f6b0857874ebde79594d6d92ad899f71836b8e9
Leonardo Carvalho <leohoc@hotmail.com>
2020-03-30 10:32:22 -0300

    Adding a prohecy dynamodb table

--- run/local.sh
+++ run/local.sh
@@ -1,7 +1,15 @@
 aws dynamodb create-table --profile local --region us-west-2 \
-  --cli-input-json file://dynamodb/tables.json \
+  --cli-input-json file://dynamodb/prophetTable.json \
   --endpoint-url http://localhost:8000
 
 aws dynamodb put-item --profile local --region us-west-2 \
-  --cli-input-json file://dynamodb/items.json \
+  --cli-input-json file://dynamodb/prophetItem.json \
+  --endpoint-url http://localhost:8000
+
+aws dynamodb create-table --profile local --region us-west-2 \
+  --cli-input-json file://dynamodb/prophecyTable.json \
+  --endpoint-url http://localhost:8000
+
+aws dynamodb put-item --profile local --region us-west-2 \
+  --cli-input-json file://dynamodb/prophecyItem.json \
   --endpoint-url http://localhost:8000
